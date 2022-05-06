208f43b8941b0b0c507c2c476ae84aa77ff7cc37
gaurav keshari <kesharigaurav97@gmail.com>
2022-05-06 17:41:06 +0530

    step percentile added

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -5,15 +5,26 @@ const Calculator = (props) => {
   const [data, setData] = useState("");
   const [percentile, setPercentile] = useState(0);
   const [output, setOutput] = useState("");
-  
+  const [checked,setChecked] = useState(false);
+  const [step_value,setStepValue] = useState([]);
+  const list=[];
+   for (const [i, val] of step_value.entries()) {
+     list.push(<tr key={i}>
+       <td>{i*5}th percentile: </td>
+       <td>{val}</td>
+     </tr>);
+   }
   async function calculatePercentile(event) {
     try {
        event.preventDefault();
       const res=await axios.post("http://localhost:8000/",{
-        data,percentile
+        data,percentile,checked
       });
-       setData(res.data.input);
+       if(res.data.input)
+       setData(res.data.input.toString());
        setOutput(res.data.ans);
+       if(res.data.step_value)
+       setStepValue(res.data.step_value);
     } catch (err) {
       console.log(err);
       setOutput("some error occured");
@@ -42,10 +53,20 @@ const Calculator = (props) => {
             required
             min={0}
             max={100}
+            step="0.01"
             onChange={(e) => {
               setPercentile(e.target.value);
             }}
           />
+          <label className='checkbox'>
+            <input
+              type='checkbox'
+              defaultChecked={checked}
+              value={checked}
+              onChange={() => setChecked(!checked)}
+            />
+            create a table of percentiles every 5%
+          </label>
         </div>
         <div className='div__btn'>
           <button
@@ -54,21 +75,33 @@ const Calculator = (props) => {
               e.preventDefault();
               setPercentile(0);
               setData("");
+              setOutput("");
+              setStepValue([]);
             }}
           >
             Clear
           </button>
-          <input 
-            className='btn'
-            type="submit"
-            value="Calculate"
-          />
+          <input className='btn' type='submit' value='Calculate' />
         </div>
       </form>
       <div className='output'>
         <h3>Output Screen</h3>
         <h3 className='output__screen'>Answer: {output}</h3>
       </div>
+      {step_value.length > 0 && (
+        <div className='step__percentile'>
+          <h2>Percentiles:</h2>
+          <table className='step__table'>
+            <thead>
+              <tr>
+                <th>Percentile</th>
+                <th>Value</th>
+              </tr>
+            </thead>
+            <tbody>{list}</tbody>
+          </table>
+        </div>
+      )}
     </div>
   );
 };
